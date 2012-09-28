3857baec5941fefc37b66aed3034f97703a599ff
Alessandro Pasotti <apasotti@gmail.com>
2012-09-28 14:35:04 +0200

    Fix link css

--- templates/accessible/index.php
+++ templates/accessible/index.php
@@ -115,8 +115,8 @@ if($fap_font_size_request = JRequest::getVar('fap_font_size')){
 <link href="<?php echo JURI::base();?>templates/<?php echo $this->template; ?>/css/skin_black.less" type="text/css" rel="stylesheet/less" />
 <script type="text/javascript" src="<?php echo JURI::base();?>templates/<?php echo $this->template;?>/js/less-1.2.1.min.js"></script>
 <?php } else { ?>
-<link href="<?php echo JURI::base();?>templates/<?php echo $this->template; ?>/css/skin_white.css" type="text/css" rel="stylesheet">
-<link href="<?php echo JURI::base();?>templates/<?php echo $this->template; ?>/css/skin_black.css" type="text/css" rel="stylesheet">
+<link href="<?php echo JURI::base();?>templates/<?php echo $this->template; ?>/css/skin_white.css" type="text/css" rel="stylesheet" />
+<link href="<?php echo JURI::base();?>templates/<?php echo $this->template; ?>/css/skin_black.css" type="text/css" rel="stylesheet" />
 <?php } ?>
 <link href="<?php echo JURI::base();?>templates/<?php echo $this->template; ?>/css/template_css.css" rel="stylesheet" type="text/css"/>
 <?php if ($this->params->get('custom_theme') == 'yes' && file_exists(JPATH_THEMES.'/'.$this->template.'/css/custom_theme.css')) { ?>
